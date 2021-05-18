647db327085ae07111d7000f2fe4d81c1768084e
Anthony Sim <anthonyysim@gmail.com>
2021-05-18 14:53:52 -0700

    installed axios set up k6 new relic tests & created localhost route to title banner service

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('newrelic');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
@@ -11,6 +12,18 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
+
+// Title Banner Routes
+app.get('/getTitle/:id', async (req, res) => {
+  await axios.get(`http://localhost:3001/getTitle/${req.params.id}`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log('Error with GET request to server: images', err.address);
+      res.status(404).end();
+    });
 });
 
 // Server Connection
